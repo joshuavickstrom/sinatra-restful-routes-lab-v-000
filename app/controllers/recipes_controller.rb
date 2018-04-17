c981ef63172d1cff2cf44ab39ee730910a103f22
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-17 21:05:58 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,9 @@ class RecipesController < ApplicationController
     erb :"recipes/index"
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :"recipes/show"
