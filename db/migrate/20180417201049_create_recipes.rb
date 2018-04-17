f13d46398b685d2439306faf9e6bcaf1771088a4
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-17 20:12:32 +0000

    automatically backed up by learn

--- db/migrate/20180417201049_create_recipes.rb
+++ db/migrate/20180417201049_create_recipes.rb
@@ -1,6 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
-    create_table recipes do |t|
+    create_table :recipes do |t|
       t.string :name
       t.string :ingredients
       t.string :cook_time
